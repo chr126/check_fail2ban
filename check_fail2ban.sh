b3576389b9711005fc64dc9786a1496366406866
chr126 <c.rogge@atlantismedia.de>
2015-12-04 15:03:37 +0100

    Spaces replaced by tabs

--- check_fail2ban.sh
+++ check_fail2ban.sh
@@ -5,10 +5,10 @@
 #Include nominal value from .inc file
 fail2ban_inc="/etc/fail2ban/fail2ban_nagios.inc"
 if [ -f ${fail2ban_inc} ]; then
-        source ${fail2ban_inc}
+		source ${fail2ban_inc}
 else
-        echo "WARNING - Config not found!"
-        exit 1
+		echo "WARNING - Config not found!"
+		exit 1
 fi
 
 chains_actual=`/usr/bin/sudo /sbin/iptables --list -n | /bin/egrep "Chain fail2ban|Chain f2b" | /usr/bin/wc -l`
